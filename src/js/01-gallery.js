6cb314c111b5a61790f5b4ca1339442ed15b163a
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-04-28 15:27:57 +0200

    gallery/vimeo player done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+// Change code below this line
+const galleryList = document.querySelector('.gallery');
+
+const itemsGallery = galleryItems
+  .map(
+    item => `
+    <li class="gallery__item">
+    <a
+    class="gallery__link"
+    href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"/>
+      </a>
+    </li>
+`
+  )
+  .join('');
+galleryList.innerHTML = itemsGallery;
+// console.log(itemsGallery);
+// console.log(galleryItems);
+
+galleryList.addEventListener('click', onImageClick);
+
+function onImageClick(eve) {
+  eve.preventDefault();
+
+  const clickElem = eve.target;
+  //   console.log(clickElem);
+
+  if (clickElem.nodeName !== 'IMG') {
+    return;
+  }
+  const originImage = clickElem.dataset.source;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
